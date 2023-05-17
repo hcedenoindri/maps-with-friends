5d618e7a41b6849307dea536bfa1c931b51f1da5
hcedenoindri <hcedenoindriago@gmail.com>
2023-05-17 18:57:48 -0500

    update chatgpt func

--- src/Game/chatgpt.ts
+++ src/Game/chatgpt.ts
@@ -2,6 +2,6 @@ import { Configuration, OpenAIApi } from "openai";
 
 export const openai = new OpenAIApi(
   new Configuration({
-    apiKey: "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",
+    apiKey: "",
   })
 );
